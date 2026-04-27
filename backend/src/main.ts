d892f4be81250d76ac4380407710fab855b28e9b
jgouvea7 <jonnathasg@gmail.com>
2026-04-27 17:25:30 -0300

    fix: test startup azure v2

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -45,7 +45,7 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api');
 
-  const port = process.env.PORT ?? 3000;
-  await app.listen(port);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
