39b63ecb79b8b2e578ef6c99207d3fc6e6ade6f7
jgouvea7 <jonnathasg@gmail.com>
2026-05-07 18:28:30 -0300

    fix: url DB

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -55,7 +55,7 @@ import { StockMovementsModule } from './stock-movements/stock-movements.module';
         }),
 
       autoLoadEntities: true,
-      synchronize: true,
+      synchronize: (process.env.DB_SYNCHRONIZE ?? 'false') === 'true',
       logging: true,
     }),
     UsersModule,
